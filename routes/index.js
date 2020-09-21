0def4a4a9c8a69dd33b20025ab03241d8a8348af
Nikhil Jha <exilednick@outlook.com>
2020-09-21 11:27:26 +0530

    Removed dead links from homepage

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,10 @@ const router = express.Router();
 
 router.get('/subs/meeting-room', (req,res) => {
   res.render('editor');
-})
+});
 
 router.get('/', function(req, res) {
   res.render('index');
-})
+});
 
 module.exports = router;
